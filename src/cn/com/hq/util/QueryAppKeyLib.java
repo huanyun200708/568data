b36c10874af0e9f207d2faca7016aa92a63b7efb
huangqi <huangqi200708>
2017-08-31 17:14:31 +0800

    add query

---
+++ src/cn/com/hq/util/QueryAppKeyLib.java
@@ -0,0 +1,18 @@
+package cn.com.hq.util;
+
+public class QueryAppKeyLib {
+	//邮编查询
+	public static String postcodeQueryAppKey = "c02cb2b12c940e8fb10000b319b15b10";
+	public static String postcodeQueryUrl = "http://v.juhe.cn/postcode/query?";
+	//车辆信息查询
+	public static String carInfoQueryAppKey = "c54b7bc1605c7aec1917cb352e251470";
+	public static String carInfoQueryUrl = "http://v.juhe.cn/carInfo/appendix.php?";
+	//车辆信息和去年投保信息
+	public static String carInsuranceInfoQueryAppKey = "095c75d971ce0d7016cfb4f4e4db7e7d";
+	public static String carInsuranceInfoQueryUrl = "http://v.juhe.cn/carInsuranceInfo/query.php?";
+	//汽车维修保养记录
+	public static String maintenanceQueryAppKey = "746692a951f1118a13ce74e9d8fb127e";
+	public static String maintenanceQueryUrl = "http://v.juhe.cn/maintenance/submitOrder.php?";
+	
+	
+}
